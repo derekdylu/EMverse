2d5665a1e21a25ef741b1b58b87bf3bc2ad776d5
derekdylu <derekdylu@users.noreply.github.com>
2022-03-03 14:52:01 +0800

    fend index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,50 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, split } from '@apollo/client';
+// import { split } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+// import dotenv from 'dotenv-defaults';
+// dotenv.config();
+
+const httpLink = new HttpLink({ 
+	uri: "http://localhost:5000/",
+});
+
+// const wsLink = new WebSocketLink ({
+// 	uri: "ws://be.circlediary.daedluz.com",
+// 	options: { reconnect: true },
+// });
+
+// using the ability to split links, you can send data to each link
+// depending on what kind of operation is being sent
+const link = split(
+	// split based on operation type
+	({ query }) => {
+	  const definition = getMainDefinition(query);
+	  return (
+		definition.kind === 'OperationDefinition' &&
+		definition.operation === 'subscription'
+	  );
+	},
+	httpLink,
+	httpLink,
+);
+  
+  const client = new ApolloClient({
+	link,
+	cache: new InMemoryCache().restore({}),
+});
 
 ReactDOM.render(
+  
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+  ,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
