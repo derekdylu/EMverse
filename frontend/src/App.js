df67e6f7b8d5077f72cbc8b84b955a3857fa735c
derekdylu <derekdylu@users.noreply.github.com>
2022-03-05 21:17:52 +0800

    Remove obsolete application routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,4 @@
 import './App.css';
-<<<<<<< HEAD
-import React, { useEffect } from "react";
-
-function App() {
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.-js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-=======
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import InputPage from './inputPage';
 
@@ -36,7 +13,6 @@ function App() {
 			</div>
 		</Router>
 	);
->>>>>>> a475696ca01c4e4be3c8262bede098d7d746e7aa
 }
 
 export default App;
