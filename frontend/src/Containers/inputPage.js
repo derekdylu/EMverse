472124724020e3649a01f6978343cb735c0f1ed8
Grace Theodore <derekdylu@users.noreply.github.com>
2022-03-30 11:36:34 +0800

    Refine comment and input pages

--- frontend/src/Containers/inputPage.js
+++ frontend/src/Containers/inputPage.js
@@ -1,10 +1,31 @@
 import './inputPage.css';
 import { React, useState, useEffect } from 'react';
+import ReactFreezeframe from 'react-freezeframe';
+import Dragabble from 'react-draggable';
 
 import { CREATE_POST,
          UPDATE_POST }
        from '../graphql';
 import { useMutation } from '@apollo/client';
+
+const title = "分享你的觀點";
+const body = "您的填答內容將會被分享於EMVerse的留言牆上，其他人有機會看到您的言論，填答時請避免冒犯他人。我們不會透露您的個人資訊，請放心填答。";
+const question = [
+    "在想些什麼呢？",
+    "感覺如何？"
+];
+const addition = "試著選出符合你的意見的情緒表情";
+/*
+    TODO:
+    [ ] change pointer on submit btn hover
+    [ ] reset page style on submit btn clicked
+    [ ] edit drag bar
+    [ ] line spacing, don't center text (when did everything goes wrong)
+    [ ] emotion subtitle, center emotion vertically (s.t. other gif size is not effected by chosen gif)
+    [ ] different page on desktop and mobile (hm?)
+    [ ] pull up page
+    [ ] must beautify code :")
+*/
 
 export default function InputPage() {
     const [post, setPost] = useState({emotion: "", text: ""});
@@ -14,49 +35,93 @@ export default function InputPage() {
         e.preventDefault();
 
         console.log(post);
-        try {
-            await createPost({ variables: post });
+        // try {
+        //     await createPost({ variables: post });
+        // }
+        // catch(e)
+        // {
+        //     console.log(e);
+        // }
+    }
+
+    function chooseEmotion(e) {
+        let prev = post.emotion;
+
+        setPost({...post, emotion: e.target.className});
+
+        if (prev === e.target.className) {
+            if (e.target.style.border === "none") {
+                e.target.style.border = "1px solid black";
+            }
+            else {
+                e.target.style.border = "none";
+            }
         }
-        catch(e)
-        {
-            console.log(e);
+        else if (prev !== "") {
+            let prevCSS = document.getElementsByClassName(prev)[0];
+
+            prevCSS.style.border = "none";
+            e.target.style.border = "1px solid black";
         }
+        else {
+            e.target.style.border = "1px solid black";
+        }
+
     }
 
     return (
-        <form className="box" onSubmit={handleSubmit}>
-            <h2>分享你的觀點</h2>
-            <h4>
-            您的填答內容將會被分享於EMVerse的留言牆上，其他人有機會看到您的言論，填答時請避免冒犯他人。我們不會透露您的個人資訊，請放心填答。
-            </h4>
-            <hr />
+        <div className="fullpage">
+        {/* <Dragabble axis="y" handle=".handle"> */}
+            <div className="box">
+            <div className="handle">
+                <div className="stick" />
+                <div className="pad title">{title}</div>
+                <div className="pad body">{body}</div>
+                {/* <hr /> */}
+            </div>
+            <div className="form">
+                <form onSubmit={handleSubmit}>
+                    <div className="pad step">step1</div>
+                    <div className="pad question">{question[0]}</div>
+                    <textarea className="textInput" placeholder="留下對議題的看法吧！" onChange={(e) => setPost({...post, text: e.target.value})} />
+                    <hr />
 
-            <h4>step1</h4>
-            <h3>在想些什麼呢？</h3>
-            <input type="text" className="textInput" placeholder="留下對議題的看法吧！" onChange={(e) => setPost({...post, text: e.target.value})}></input>
-            <hr />
+                    <div className="pad step">step2</div>
+                    <div className="question">{question[1]}</div>
+                    <div className="pad addition">{addition}</div>
 
-            <h4>step2</h4>
-            <h3>感覺如何？</h3>
-            <h4 style={{fontWeight: "bold"}}>試著選出符合你的意見的情緒表情</h4>
-            {/* <label>
-            <input type="radio" name="test" value="small" checked />
-            <img src="../haha.gif" />
-            </label> */}
+                    <div className="container">
+                        <div className="gif">
+                            <img className="HAHA" src="../haha.gif" onClick={(e) => chooseEmotion(e)} alt=""/>
+                            <div className="addition">喜悅</div>
+                        </div>
+                        <div className="gif">
+                            <img className="SAD" src="../sad.gif" onClick={(e) => chooseEmotion(e)} alt=""/>
+                            <div className="addition">悲傷</div>
+                        </div>
+                        <div className="gif">
+                            <img className="DISGUST" src="../cheer_up.gif" onClick={(e) => chooseEmotion(e)} alt=""/>
+                            <div className="addition">噁心</div>
+                        </div>
+                        <div className="gif">
+                            <img className="FEAR" src="../thinking.gif" onClick={(e) => chooseEmotion(e)} alt=""/>
+                            <div className="addition">恐懼</div>
+                        </div>
+                        <div className="gif">
+                            <img className="ANGRY" src="../angry.gif" onClick={(e) => chooseEmotion(e)} alt=""/>
+                            <div className="addition">憤怒</div>
+                        </div>
+                        <div className="gif">
+                            <img className="WOW" src="../wow.gif" onClick={(e) => chooseEmotion(e)} alt=""/>
+                            <div className="addition">驚訝</div>
+                        </div>
+                    </div>
 
-            <label><input type="radio" name="emotion" value="HAHA" onChange={(e) => setPost({...post, emotion: e.target.value})} />
-            haha</label>
-            <label><input type="radio" name="emotion" value="ANGRY" onChange={(e) => setPost({...post, emotion: e.target.value})} />
-            angry</label>
-            <label><input type="radio" name="emotion" value="SAD" onChange={(e) => setPost({...post, emotion: e.target.value})} />
-            sad</label>
-            <label><input type="radio" name="emotion" value="WOW" onChange={(e) => setPost({...post, emotion: e.target.value})} />
-            wow</label>
-            <label><input type="radio" name="emotion" value="FEAR" onChange={(e) => setPost({...post, emotion: e.target.value})} />
-            fear</label>
-            <label><input type="radio" name="emotion" value="DISGUST" onChange={(e) => setPost({...post, emotion: e.target.value})} />
-            disgust</label><br/><br/>
-            <input type="submit" className="submit" value="確認送出" />
-        </form>
+                    <input type="submit" className="submit" value="確認送出" />
+                </form>
+            </div>
+            </div>
+        {/* </Dragabble> */}
+        </div>
     )
 }
